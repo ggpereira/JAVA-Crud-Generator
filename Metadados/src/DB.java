a047954b84e5a8eab738d3af7cb707d6ba293b88
Gabriel <ggomesper@gmail.com>
2017-10-23 23:46:50 -0200

    Leitura de Metadados

---
+++ Metadados/src/DB.java
@@ -0,0 +1,7 @@
+
+public class DB {
+	public static final String JDBC_DRIVER = "org.postgresql.Driver"; 
+	static final String SENHA = "@1g2o3m4e5s";
+	static final String USUARIO = "postgres";
+	static final String URL_CONEXAO = "jdbc:postgresql://localhost/biblioteca";
+}
