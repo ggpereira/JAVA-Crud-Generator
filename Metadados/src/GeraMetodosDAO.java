0865b79578e81c208c933e971c80c5afea9e0d95
Otávio da Cruz Mello <32108475+odcmello@users.noreply.github.com>
2017-11-09 12:32:15 -0200

    Update GeraMetodosDAO.java

--- Metadados/src/GeraMetodosDAO.java
+++ Metadados/src/GeraMetodosDAO.java
@@ -4,108 +4,111 @@ import java.util.ArrayList;
 
 public class GeraMetodosDAO 
 {
+        private String DAO;
 	private String metodoInserir; 
 	private String metodoDeletar;
 	private String metodoBuscar;
 	private String metodoAlterar;
         private int contador=1;
-	
-	public Collection<String> geraResultSet(Tabela t)
-	{
-		Collection<String> result_set = new ArrayList<String>();
-		String op;
-		
-		for(Coluna column  : t.getColumns())
-		{
-			op = "rs.get" + column.getTypeName();
-			System.out.println(op);
-			result_set.add(op);
-		}
-	
-		
-		return result_set;
-	}
-	
+        
+        public String geraMetodosDAO(Tabela t){
+                DAO = "import java.sql.Connection;\n" + "import java.sql.DriverManager;\n" + "import java.sql.PreparedStatement;\n" +
+                "import java.sql.ResultSet;\n" + "import java.sql.SQLException;\n" + "import java.sql.Statement;\n" + "import java.util.ArrayList;\n" +
+                "import java.util.Collection;\n\npublic class " + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + "DAO {\n\n";
+                DAO += "\tpublic Connection open() {\n\t\tConnection c = null;\n\t\ttry {\n\t\t\tClass.forName(DB.JDBC_DRIVER);\n";
+                DAO += "\t\t} catch (ClassNotFoundException e) {\n\t\t\tSystem.out.println(\"Não encontrou o driver chamado\" + DB.JDBC_DRIVER + \"na memória.\");\n\t\t}\n";
+                DAO += "\t\ttry {\n\t\t\tc = DriverManager.getConnection(DB.URL_CONEXAO, DB.USUARIO, DB.SENHA);\n\t\t} catch (SQLException e) {\n\t\t\te.printStackTrace();\n\t\t}\n";
+                DAO += "\t\treturn c;\n\t}\n";
+                DAO += geraInsert(t);
+                DAO += "\n\n" + geraDelete(t);
+                DAO += "\n\n" + geraSearch(t);
+                DAO += "\n\n" + geraUpdate(t) + "\n}";
+                return DAO;
+        }
 	
 	public String geraInsert(Tabela t)
 	{
+                contador=1;
 		GeraSQL sql = new GeraSQL();
-		metodoInserir = "public void inserir(" + t.getTableName() + " t) {\n";
-		metodoInserir += "\tConnection conexao = open();\n";
-		metodoInserir += "\ttry {\n\t\tPreparedStatement ps = conexao.prepareStatement(\"" + sql.createInsert(t) + "\");\n";
+		metodoInserir = "\tpublic void inserir(" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + " t) {\n";
+		metodoInserir += "\t\tConnection conexao = open();\n";
+		metodoInserir += "\t\ttry {\n\t\t\tPreparedStatement ps = conexao.prepareStatement(\"" + sql.createInsert(t) + "\");\n";
                 for(Coluna column : t.getColumns())					
 		{			
-                    metodoInserir += "\t\tps.set" + column.getTypeName() + "(" + contador + ",t.get" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "());\n";
+                    metodoInserir += "\t\t\tps.set" + column.getTypeName().substring(0,1).toUpperCase() + column.getTypeName().substring(1) + "(" + contador + ",t.get" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "());\n";
                     contador++;
                 }
-                metodoInserir += "\t\tps.execute();\n";
-                metodoInserir += "\t\tps.close();\n";
-                metodoInserir += "\t\tconexao.close();\n";
-                metodoInserir += "\t} catch (SQLException e) {\n";
-                metodoInserir += "\t\te.printStackTrace();\n\t}\n}";	
+                metodoInserir += "\t\t\tps.execute();\n";
+                metodoInserir += "\t\t\tps.close();\n";
+                metodoInserir += "\t\t\tconexao.close();\n";
+                metodoInserir += "\t\t} catch (SQLException e) {\n";
+                metodoInserir += "\t\t\te.printStackTrace();\n\t\t}\n\t}";	
 		return metodoInserir;
 	}
         
         public String geraUpdate(Tabela t)
         {
+                contador=1;
                 GeraSQL sql = new GeraSQL();
-                metodoAlterar = "public void alterar(" + t.getTableName() + " t) {\n";
-		metodoAlterar += "\tConnection conexao = open();\n";
-		metodoAlterar += "\ttry {\n\t\tPreparedStatement ps = conexao.prepareStatement(\"" + sql.createUpdate(t) + "\");\n";
+                metodoAlterar = "\tpublic void alterar(" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + " t) {\n";
+		metodoAlterar += "\t\tConnection conexao = open();\n";
+		metodoAlterar += "\t\ttry {\n\t\t\tPreparedStatement ps = conexao.prepareStatement(\"" + sql.createUpdate(t) + "\");\n";
                 for(Coluna column : t.getColumns())					
 		{			
-                    metodoAlterar += "\t\tps.set" + column.getTypeName() + "(" + contador + ",t.get" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "());\n";
+                    metodoAlterar += "\t\t\tps.set" + column.getTypeName().substring(0,1).toUpperCase() + column.getTypeName().substring(1) + "(" + contador + ",t.get" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "());\n";
                     contador++;
                 }
-                metodoAlterar += "\t\tps.execute();\n";
-                metodoAlterar += "\t\tps.close();\n";
-                metodoAlterar += "\t\tconexao.close();\n";
-                metodoAlterar += "\t} catch (SQLException e) {\n";
-                metodoAlterar += "\t\te.printStackTrace();\n\t}\n}";	
+                metodoAlterar += "\t\t\tps.execute();\n";
+                metodoAlterar += "\t\t\tps.close();\n";
+                metodoAlterar += "\t\t\tconexao.close();\n";
+                metodoAlterar += "\t\t} catch (SQLException e) {\n";
+                metodoAlterar += "\t\t\te.printStackTrace();\n\t\t}\n\t}";	
 		return metodoAlterar;
         }
         
         public String geraSearch(Tabela t)
         {
+                contador=1;
                 GeraSQL sql = new GeraSQL();
-                metodoBuscar = "public void buscar(" + t.getTableName() + " t) {\n";
-		metodoBuscar += "\tConnection conexao = open();\n";
-                metodoBuscar += "\tCollection<" + t.getTableName() + "> " + t.getTableName() + " = new ArrayList<" + t.getTableName() + ">();\n";
-		metodoBuscar += "\ttry {\n\t\tStatement s = conexao.createStatement();\n";
-                metodoBuscar += "\t\tResultSet rs = s.executeQuery(\"" + sql.createSelect(t) + "\");\n";
-                metodoBuscar += "\t\twhile(rs.next()) {\n\t\t\t" + t.getTableName() + " temp = new " + t.getTableName() + "();\n";
+                metodoBuscar = "\tpublic Collection<" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + "> buscar() {\n";
+		metodoBuscar += "\t\tConnection conexao = open();\n";
+                metodoBuscar += "\t\tCollection<" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + "> " + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + " = new ArrayList<" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + ">();\n";
+		metodoBuscar += "\t\ttry {\n\t\t\tStatement s = conexao.createStatement();\n";
+                metodoBuscar += "\t\t\tResultSet rs = s.executeQuery(\"" + sql.createSelect(t) + "\");\n";
+                metodoBuscar += "\t\t\twhile(rs.next()) {\n\t\t\t\t" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + " temp = new " + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + "();\n";
                 for(Coluna column : t.getColumns())					
 		{			
-                    metodoBuscar += "\t\t\ttemp.set" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "(rs.get" + column.getTypeName() + "(\"" + column.getColumnName() + "\");\n";
+                    metodoBuscar += "\t\t\t\ttemp.set" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "(rs.get" + column.getTypeName().substring(0,1).toUpperCase() + column.getTypeName().substring(1) + "(\"" + column.getColumnName() + "\"));\n";
                     contador++;
                 }
-                metodoBuscar += "\t\t\t" + t.getTableName() + ".add(temp);\n\t\t}\n";
-                metodoBuscar += "\t\trs.close();\n";
-                metodoBuscar += "\t\tconexao.close();\n";
-                metodoBuscar += "\t} catch (SQLException e) {\n";
-                metodoBuscar += "\t\te.printStackTrace();\n\t}\n";	
-                metodoBuscar += "\treturn " + t.getTableName() + ";\n}";
+                metodoBuscar += "\t\t\t\t" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + ".add(temp);\n\t\t\t}\n";
+                metodoBuscar += "\t\t\trs.close();\n";
+                metodoBuscar += "\t\t\tconexao.close();\n";
+                metodoBuscar += "\t\t} catch (SQLException e) {\n";
+                metodoBuscar += "\t\t\te.printStackTrace();\n\t\t}\n";	
+                metodoBuscar += "\t\treturn " + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + ";\n\t}";
 		return metodoBuscar;
         }
         
         public String geraDelete(Tabela t)
         {
+                contador=1;
                 GeraSQL sql = new GeraSQL();
-                metodoDeletar = "public void deletar(" + t.getTableName() + " t) {\n";
-		metodoDeletar += "\tConnection conexao = open();\n";
-                metodoDeletar += "\ttry {\n\t\tPreparedStatement ps = conexao.prepareStatement(\"" + sql.createDelete(t) + "\");\n";
+                metodoDeletar = "\tpublic void deletar(" + t.getTableName().substring(0,1).toUpperCase() + t.getTableName().substring(1) + " t) {\n";
+		metodoDeletar += "\t\tConnection conexao = open();\n";
+                metodoDeletar += "\t\ttry {\n\t\t\tPreparedStatement ps = conexao.prepareStatement(\"" + sql.createDelete(t) + "\");\n";
                 for(Coluna column : t.getColumns())					
 		{			
                     if(column.getColumnName().equals(t.getPrimaryKey())){
-                        metodoDeletar += "\t\tps.set" + column.getTypeName() + "(" + contador + ",t.get" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "());\n";
+                        metodoDeletar += "\t\t\tps.set" + column.getTypeName().substring(0,1).toUpperCase() + column.getTypeName().substring(1) + "(" + contador + ",t.get" + column.getColumnName().substring(0,1).toUpperCase() + (column.getColumnName()).substring(1) + "());\n";
                     }
                     contador++;
                 }
-                metodoDeletar += "\t\tps.execute();\n";
-                metodoDeletar += "\t\tps.close();\n";
-                metodoDeletar += "\t\tconexao.close();\n";
-                metodoDeletar += "\t} catch (SQLException e) {\n";
-                metodoDeletar += "\t\te.printStackTrace();\n\t}\n}";	
+                metodoDeletar += "\t\t\tps.execute();\n";
+                metodoDeletar += "\t\t\tps.close();\n";
+                metodoDeletar += "\t\t\tconexao.close();\n";
+                metodoDeletar += "\t\t} catch (SQLException e) {\n";
+                metodoDeletar += "\t\t\te.printStackTrace();\n\t\t}\n\t}";	
                 return metodoDeletar;
         }
 }
