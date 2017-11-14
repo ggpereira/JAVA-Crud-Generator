1be608b4cdf5dc32618bfe821bba703af51edd54
Otávio da Cruz Mello <32108475+odcmello@users.noreply.github.com>
2017-11-14 18:04:30 -0200

    Create GeraExemplo.java

---
+++ Metadados/src/GeraExemplo.java
@@ -0,0 +1,54 @@
+public class GeraExemplo {
+
+    private String geraExemplo;
+    private String geraExemploInsert;
+    private String geraExemploSearch;
+    private String geraExemploDelete;
+    private String geraExemploUpdate;
+
+    public String geraExemplo(Tabela t) {
+        geraExemplo = "public class " + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "Exemplo {\n\tpublic static void  main(String[] args) {\n";
+        geraExemplo += "\t\t" + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "DAO cs = new " + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "DAO();\n";
+        geraExemplo += geraExemploInsert(t) + "\n";
+        geraExemplo += geraExemploSearch(t) + "\n";
+        geraExemplo += geraExemploUpdate(t) + "\n";
+        geraExemplo += geraExemploDelete(t) + "\n";
+        geraExemplo += "\t}\n}";
+        return geraExemplo;
+    }
+
+    public String geraExemploInsert(Tabela t) {
+        geraExemploInsert = "\n\t\tSystem.out.println(\"Inserindo " + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "...\");\n";
+        geraExemploInsert += "\t\t" + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + " t1 = new " + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "();";
+        geraExemploInsert += "\n\t\tcs.inserir(t1);";
+        return geraExemploInsert;
+    }
+
+    public String geraExemploSearch(Tabela t) {
+        
+        geraExemploSearch = "\n\t\tSystem.out.println(\"Buscando " + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "...\");\n";
+        geraExemploSearch += "\t\tfor (" + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + " temp : cs.buscar()){\n";
+        for (Coluna column : t.getColumns()) {
+            geraExemploSearch += "\t\t\tSystem.out.println(temp.get" + column.getColumnName().substring(0, 1).toUpperCase() + column.getColumnName().substring(1) + "());\n";
+        }
+        geraExemploSearch += "\n\t\t};";
+        return geraExemploSearch;
+    }
+
+    public String geraExemploUpdate(Tabela t) {
+        geraExemploUpdate = "\n\t\tSystem.out.println(\"Alterando " + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "...\");\n";
+        for (Coluna column : t.getColumns()) {
+            geraExemploUpdate += "\t\tt1.set" + column.getColumnName().substring(0, 1).toUpperCase() + column.getColumnName().substring(1) + "(" + column.getTypeName() + ");\n";
+        }
+        geraExemploUpdate += "\t\tcs.alterar(t1);";
+        return geraExemploUpdate;
+    }
+
+    public String geraExemploDelete(Tabela t) {
+        geraExemploDelete = "\n\t\tSystem.out.println(\"Apagando todos " + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + "...\");\n";
+        geraExemploDelete += "\t\tfor (" + t.getTableName().substring(0, 1).toUpperCase() + t.getTableName().substring(1) + " temp : cs.buscar())\n";
+        geraExemploDelete += "\t\t\tcs.deletar(temp);";
+        return geraExemploDelete;
+    }
+
+}
