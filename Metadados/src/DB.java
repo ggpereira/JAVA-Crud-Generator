710554ff2e29926435d437473a4c48091b3dd2f0
Gabriel Gomes <ggomesper@gmail.com>
2017-11-24 09:10:09 -0200

    Update DB.java

--- Metadados/src/DB.java
+++ Metadados/src/DB.java
@@ -3,6 +3,6 @@ public class DB /*Necessário alterar de acordo com as informações de acesso a
 {
 	public static final String JDBC_DRIVER = "org.postgresql.Driver"; 
 	static final String SENHA = ""; /*Necessário completar*/
-	static final String USUARIO = "postgres"; /*Necessário alterar*/
+	static final String USUARIO = "postgres"; /*Necessário alterar se for preciso*/
 	static final String URL_CONEXAO = "jdbc:postgresql://localhost/biblioteca"; /*Necessário talvez alterar*/
 }
