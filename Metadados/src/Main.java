a65f0bf05c6f32b2ece6544a6ae73db4fd729f87
Gabriel Gomes <ggomesper@gmail.com>
2017-11-24 08:55:55 -0200

    Comentários adicionados

--- Metadados/src/Main.java
+++ Metadados/src/Main.java
@@ -2,14 +2,21 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
+
+/*T2 - PARADIGMAS DE PROGRAMAÇÃO - GERADOR DE CRUD
+Nomes:Gabriel Gomes Pereira, Otávio da Cruz Mello
+Turma:Sistemas de Informação 
+*/
+
 public class Main 
 {
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		
+		/*Lista de tabelas encontradas no banco de dados*/
 		Collection<Tabela> t = new ArrayList<Tabela>();
 		Metadados m = new Metadados();
 		
+		/*Recebe as tabelas preenchidas*/
 		t = m.tablesMetadata();
 		
 		for(Tabela table : t) /*Escreve as classes no arquivo*/
